1c21424a30c111a933dfdca0254a8cf4ea6d92c2
Zenway <zenway15@gmail.com>
2025-09-18 20:15:20 -0500

    feat: Se añade la ruta de docs para este micro.

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/zenway/api/security/SecurityConfig.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/zenway/api/security/SecurityConfig.java
@@ -36,7 +36,8 @@ public class SecurityConfig {
                                 "/api/v1/login",
                                 "/webjars/swagger-ui/**",
                                 "/v3/api-docs/**",
-                                "swagger-ui/**"
+                                "swagger-ui/**",
+                                "/swagger-ui.html"
                         ).permitAll()
                         .pathMatchers(HttpMethod.POST,"/api/v1/usuarios").hasAnyRole("ADMINISTRADOR", "ASESOR")
                         .pathMatchers("/api/v1/usuarios/email/{documento}").hasRole("CLIENTE")
@@ -44,8 +45,7 @@ public class SecurityConfig {
                         .anyExchange().authenticated()
                 )
                 .exceptionHandling(ex ->
-                        ex.accessDeniedHandler(reactiveSecurityExceptionHandler)
-                                .authenticationEntryPoint(reactiveSecurityExceptionHandler))
+                        ex.accessDeniedHandler(reactiveSecurityExceptionHandler))
                 .oauth2ResourceServer(oauth2 ->
                         oauth2.jwt(jwt -> jwt.jwtDecoder(jwtDecoder)
                                 .jwtAuthenticationConverter(jwtAuthenticationConverter))
